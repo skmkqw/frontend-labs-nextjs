13e200578710cac7c4a5f5f4c79451b10d228f44
skmkqw <tkorsakov77@gmail.com>
2025-12-15 00:13:54 +0100

    replace hardcoded values with env variables

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -8,11 +8,11 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBPgYEXGAI-CQ97JmXAxkESytvV-GK0fE8",
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: "frontend-labs-nextjs.firebaseapp.com",
     projectId: "frontend-labs-nextjs",
     storageBucket: "frontend-labs-nextjs.firebasestorage.app",
-    messagingSenderId: "23925096192",
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGIN_SENDER_ID,
     appId: "1:23925096192:web:d093303e2615303b403de5",
     measurementId: "G-PW1WY79BE0"
 };
