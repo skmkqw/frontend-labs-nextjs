7be7827e5df919ecf099b85c88f4ce5da4c34405
skmkqw <tkorsakov77@gmail.com>
2025-12-14 23:48:41 +0100

    fix not found page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
         <div className="h-screen m-auto flex justify-center items-center">
             <div className="max-w-xs overflow-hidden rounded-lg shadow-lg bg-gray-700">
                 <div className="px-6 py-4">
-                    <h4 className="mb-3 text-2xl font-semibold">
+                    <h4 className="mb-3 text-2xl font-semibold text-white">
                         Nie znaleziono strony
                     </h4>
-                    <p className="leading-normal">
+                    <p className="leading-normal text-white">
                         Wygląda na to, że adres jest nieprawidłowy Wróć do
                         strony głównej, aby kontynuować.
                     </p>
@@ -16,7 +16,7 @@ export default function NotFound() {
                     <Button
                         variant="link"
                         href="/"
-                        className="w-full mt-2 text-lg py-6"
+                        className="w-full mt-4 text-lg py-6"
                     >
                         Wróć
                     </Button>
