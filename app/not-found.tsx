7da7186243db30515332c277fba0226efa58377f
skmkqw <tkorsakov77@gmail.com>
2025-12-14 22:59:20 +0100

    create not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import Button from "./components/button";
+
+export default function NotFound() {
+    return (
+        <div className="h-screen m-auto flex justify-center items-center">
+            <div className="max-w-xs overflow-hidden rounded-lg shadow-lg bg-gray-700">
+                <div className="px-6 py-4">
+                    <h4 className="mb-3 text-2xl font-semibold">
+                        Nie znaleziono strony
+                    </h4>
+                    <p className="leading-normal">
+                        Wygląda na to, że adres jest nieprawidłowy Wróć do
+                        strony głównej, aby kontynuować.
+                    </p>
+
+                    <Button
+                        variant="link"
+                        href="/"
+                        className="w-full mt-2 text-lg py-6"
+                    >
+                        Wróć
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
